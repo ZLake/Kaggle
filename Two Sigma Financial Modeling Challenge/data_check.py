14ab18cf32ab3633ffaa4ca41dc643ee0d887358
lake.zl <lake.zl@alibaba-inc.com>
2017-12-26 10:47:32 +0800

    update

---
+++ Two Sigma Financial Modeling Challenge/data_check.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Wed Sep 13 16:22:54 2017
+
+@author: lakezhang
+"""
+import pandas as pd
+# read the data:
+train = pd.read_hdf('input/train.h5')
+
+print('Finished...')
